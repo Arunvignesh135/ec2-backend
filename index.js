fc1ff1bb4130067ffd97f5653b0016f423a839e2
Arunvignesh135 <arunvickyavm@gmail.com>
2026-01-23 12:21:24 +0530

    Update server listen address to '0.0.0.0'

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log("Server started on port", PORT);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
